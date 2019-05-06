324df56707b349aa7271f780c566b6cb0eb22943
Lucas Vocos <lucas.vocos@me.com>
2019-05-06 11:45:05 -0400

    fixed html file

--- scripts/store.js
+++ scripts/store.js
@@ -5,12 +5,6 @@ const store = (function(){
   const items = [];
   let hideCheckedItems = false;
   let searchTerm = '';
-  // items: [
-  //   { id: cuid(), name: 'apples', checked: false },
-  //   { id: cuid(), name: 'oranges', checked: false },
-  //   { id: cuid(), name: 'milk', checked: true },
-  //   { id: cuid(), name: 'bread', checked: false }
-  // ],
 
 
   return {
